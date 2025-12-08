7ddb70727b2a330abc660eba4e21c47f28023a8b
Ella0921 <yinwentsai@gmail.com>
2025-12-07 19:21:58 -0800

    Add lowstock test

---
+++ test/test_suite.py
@@ -0,0 +1,27 @@
+import unittest
+
+from .test_items import TestItems
+from .test_operations import TestOperations
+from .test_expiry import TestExpiry
+from .test_lowstock import TestLowStock
+
+
+def suite():
+    """
+    Build a test suite that includes all test classes
+    for the FreshFridge package.
+    """
+    loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+
+    test_suite.addTests(loader.loadTestsFromTestCase(TestItems))
+    test_suite.addTests(loader.loadTestsFromTestCase(TestOperations))
+    test_suite.addTests(loader.loadTestsFromTestCase(TestExpiry))
+    test_suite.addTests(loader.loadTestsFromTestCase(TestLowStock))
+
+    return test_suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
